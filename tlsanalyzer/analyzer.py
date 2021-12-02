a1afe9427a7f6e6bd9710e5cd65741a3472fe231
Ilja Rozhko <ilja@oktobyte.com>
2021-12-02 14:32:16 +0100

    + Added info plist analyzer

--- tlsanalyzer/analyzer.py
+++ tlsanalyzer/analyzer.py
@@ -1,6 +1,8 @@
 import logging
+import os.path
 
 from tlsanalyzer.extractor import Extractor
+from tlsanalyzer.modules.info_plist_analyzer import InfoPlistAnalyzer
 
 
 class Analyzer:
@@ -8,6 +10,7 @@ class Analyzer:
     def __init__(self, work_dir, ipa_file):
         self.work_dir = work_dir
         self.ipa_file = ipa_file
+        self.info_plist_results = {}
         self.analyze()
 
     def analyze(self):
@@ -18,3 +21,17 @@ class Analyzer:
             return
 
         logging.info(f'Starting to analyze {self.ipa_file}...')
+        self.info_plist_results = self.analyze_info_plist(app_path)
+
+    def analyze_info_plist(self, app_path):
+        info_plist_path = os.path.join(app_path, 'Info.plist')
+        if not os.path.isfile(info_plist_path):
+            logging.warning('App does not contain a Info.plist. Skipping .plist checks.')
+            return False
+
+        analyzer = InfoPlistAnalyzer(info_plist_path)
+        return analyzer.analyze()
+
+    def ats_exceptions(self):
+        return self.info_plist_results['ats']
+
---
+++ tlsanalyzer/modules/info_plist_analyzer.py
@@ -0,0 +1,37 @@
+from plistlib import (load)
+from tlsanalyzer.modules.ats.ats_plist_analyzer import (
+    check_transport_security
+)
+
+
+class InfoPlistAnalyzer:
+
+    def __init__(self, info_plist_file):
+        self.info_plist_file = info_plist_file
+
+    def analyze(self):
+        plist_info = {
+            'name': '',
+            'binary': '',
+            'bundle_identifier': '',
+            'version': '',
+            'build': '',
+            'sdk': '',
+            'min_os': '',
+            'permissions': {},
+            'ats': []
+        }
+
+        with open(self.info_plist_file, 'rb') as fp:
+            plist_obj = load(fp)
+
+        plist_info['name'] = (plist_obj.get('CFBundleDisplayName', '') or plist_obj.get('CFBundleName', ''))
+        plist_info['binary'] = plist_obj.get('CFBundleExecutable', '')
+        plist_info['bundle_identifier'] = plist_obj.get('CFBundleIdentifier', '')
+        plist_info['version'] = plist_obj.get('CFBundleShortVersionString', '')
+        plist_info['build'] = plist_obj.get('CFBundleVersion', '')
+        plist_info['sdk'] = plist_obj.get('DTSDKName', '')
+        plist_info['min_os'] = plist_obj.get('MinimumOSVersion', '')
+        plist_info['ats'] += check_transport_security(plist_obj)
+
+        return plist_info
