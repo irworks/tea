0ab41a1cd7db195d3abdd484f5873f750c88e3fe
Ilja Rozhko <ilja@oktobyte.com>
2021-12-02 14:32:27 +0100

    ~ Output results as json file

--- tlsanalyzer/main.py
+++ tlsanalyzer/main.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 from tlsanalyzer.collector import Collector
 from tlsanalyzer.analyzer import Analyzer
@@ -15,6 +16,18 @@ def main():
     except FileNotFoundError:
         return
 
+    insecure_apps = []
     for app in apps:
         analyzer = Analyzer(work_dir, app)
+        if analyzer.ats_exceptions():
+            insecure_apps.append(analyzer.info_plist_results)
+
+    logging.info('')
+    logging.info('--- Analysis complete ---')
+    logging.info(f'{len(insecure_apps)} / {len(apps)} apps have ATS exceptions')
+
+    results = open('results.json', 'w')
+    # magic happens here to make it pretty-printed
+    results.write(json.dumps(insecure_apps))
+    results.close()
 
