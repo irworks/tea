a2b731472d8068c8885d8b66a7d5699588a7cdb7
Ilja Rozhko <ilja@oktobyte.com>
2021-12-08 17:51:17 +0100

    + Added easy sorting stuff

--- visualizer/src/js/components/AppList.vue
+++ visualizer/src/js/components/AppList.vue
@@ -10,8 +10,8 @@
       <th scope="col">Name</th>
       <th scope="col">Version</th>
       <th scope="col">iOS</th>
-      <th scope="col">ATS Exceptions</th>
-      <th scope="col">URLs</th>
+      <th scope="col">ATS Exceptions <span @click="sort('ats')">{{ icon('ats') }}</span></th>
+      <th scope="col">URLs <span @click="sort('urls')">{{ icon('urls') }}</span></th>
       <th scope="col">Details</th>
     </tr>
     </thead>
@@ -39,10 +39,33 @@ export default {
     return {
       apps: {},
       currentApp: null,
+      sortOrder: {
+        'ats': true,
+        'urls': true
+      },
       count: 0
     }
   },
   methods: {
+    icon(field) {
+      if (!this.sortOrder.hasOwnProperty(field)) {
+        return '';
+      }
+
+      return this.sortOrder[field] ? '⬆️' : '⬇️';
+    },
+    sort(field) {
+      this.sortOrder[field] = !this.sortOrder[field];
+      let up = this.sortOrder[field];
+
+      this.apps.sort((a, b) => {
+        if (up) {
+          return a[field].length > b[field].length;
+        }
+
+        return a[field].length < b[field].length;
+      });
+    },
     fetchApps() {
       this.loadJsonResults((response) => {
         // Parse JSON string into object
