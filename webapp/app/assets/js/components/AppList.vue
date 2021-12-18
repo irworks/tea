1f797b319223d68fc94e13c7e12161724cd84184
Ilja Rozhko <ilja@oktobyte.com>
2021-12-18 12:42:57 +0100

    - Remove url list from main table for now

--- webapp/app/assets/js/components/AppList.vue
+++ webapp/app/assets/js/components/AppList.vue
@@ -11,7 +11,6 @@
       <th scope="col">Version</th>
       <th scope="col">iOS</th>
       <th scope="col">ATS Exceptions <span @click="sort('ats')">{{ icon('ats') }}</span></th>
-      <th scope="col">URLs <span @click="sort('urls')">{{ icon('urls') }}</span></th>
       <th scope="col">Details</th>
     </tr>
     </thead>
@@ -22,7 +21,6 @@
       <td>{{ app.version }} ({{ app.build }})</td>
       <td>iOS {{ app.min_ios }}+</td>
       <td>{{ app.ats }}</td>
-      <td>{{ 0 }}</td>
       <td><button class="btn btn-primary" @click="selectApp(app)">Details</button></td>
     </tr>
     </tbody>
